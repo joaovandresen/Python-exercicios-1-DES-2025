aa30ea21bed6d7dc075ddd9ad7996246c51a1c6f
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-05-29 16:14:48 +0000

    exercicio 08 finalizado

--- exercicio02.py
+++ exercicio02.py
@@ -2,4 +2,3 @@
 # para finalizar três tarefas: X, Y e Z.
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
-
--- exercicio08.py
+++ exercicio08.py
@@ -1,7 +1,10 @@
-# Talita está simulando o custo de frete para sua loja virtual. O valor depende da distância:
+distancia = float(input("Digite a distância em km: "))
 
-# Até 50 km: R$ 5,00
-# De 51 a 150 km: R$ 15,00
-# Acima de 150 km: R$ 25,00
-
-
+if distancia <= 50:
+    frete = 5.00  
+elif distancia <= 150:
+    frete = 15.00  
+else:
+    frete = 25.00  
+print(f"O custo do frete para {distancia} km é R$ {frete:.2f}")
+#FINALIZADO

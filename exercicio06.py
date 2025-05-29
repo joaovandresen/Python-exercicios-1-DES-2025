af150092c6f858a9279812c5696f24c66913dd4e
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-05-29 14:22:03 +0000

    exercicio06corrigido

--- exercicio06.py
+++ exercicio06.py
@@ -1,12 +1,7 @@
 acesso_plataforma = int(input("Digite o horário que você está acessando a plataforma: "))
-print = ("Acesso permitido.")
 
-if acesso_plataforma >= 8 :
+if acesso_plataforma <= 22 :
    print("Alerta! Você está tentando acessar a plataforma em um horário não permitido.")
-else:
-   print("Horário não autorizado.")
 
-if acesso_plataforma >= 22 :
-   print("Alerta! Você está tentando acessar a plataforma em um horário não permitido.")
-else:
-   print("Horário não autorizado.")
+if acesso_plataforma >= 9 :
+   print("Horário autorizado.")

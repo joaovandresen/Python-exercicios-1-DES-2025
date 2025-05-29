4879163f3b1c0aa32624946ae91e1b9b0a291535
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-05-29 16:16:27 +0000

    exercicio 07 finalizado

--- exercicio07.py
+++ exercicio07.py
@@ -1,8 +1,11 @@
-# Uma empresa quer avaliar seus colaboradores com base em três metas atingidas.
-# O programa deve calcular a média das três avaliações e exibir:
+nota1 = int(input("Digite a nota da primeira meta: "))
+nota2 = int(input("Digite a nota da segunda meta: "))
+nota3 = int(input("Digite a nota da terceira meta: "))
 
-# Aprovado (>=7)
-# Em treinamento (>=5 e <7)
-# Reprovado (<5)
-
-
+media = (nota1 + nota2 + nota3) 
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Em treinamento")
+else:
+    print("Reprovado")

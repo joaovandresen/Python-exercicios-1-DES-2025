8799efb9c59c510f24afb6362b47a62179fdedfe
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-03 13:57:45 +0000

    exercicio 02 finalizado

--- exercicio02.py
+++ exercicio02.py
@@ -1,10 +1,10 @@
-tempo_x = int(input("Digite o tempo da tarefa X (em horas): "))
-tempo_y = int(input("Digite o tempo da tarefa Y (em horas): "))
-tempo_z = int(input("Digite o tempo da tarefa Z (em horas): "))
+dias_01 = int(input("Digite o tempo da tarefa X: "))
+dias_02 = int(input("Digite o tempo da tarefa Y: "))
+dias_03 = int(input("Digite o tempo da tarefa Z: "))
 
-if tempo_x < 0 or tempo_y < 0 or tempo_z < 0:
+if dias_01 < 0 or dias_02 < 0 or dias_03 < 0:
     print("Erro: tempo de tarefa negativo!")
 else:
-    total = tempo_x + tempo_y + tempo_z
+    total = dias_01 + dias_02 + dias_03
     print("Tempo total para finalizar as tarefas:", total, "horas")
 #FINALIZADO

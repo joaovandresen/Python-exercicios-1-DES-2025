0386d0401d7900a4108925a54739dfbbad93549d
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-12 14:33:54 +0000

    exercicio 16 finalizado

--- exercicio16.py
+++ exercicio16.py
@@ -2,3 +2,12 @@
 # Salários até R$ 2000,00: +15%
 # De R$ 2000,01 a R$ 5000,00: +10%
 # Acima de R$ 5000,00: +5%
+salario = float(input("Digite o seu salário: "))
+if salario <= 2000:
+    salario_novo = salario * 1.15
+elif salario <= 5000:
+    salario_novo = salario * 1.10
+else:
+    salario_novo = salario * 1.05
+print(f"O seu salário é: R$ {salario_novo:.2f}")
+#FINALIZADO

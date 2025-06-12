9fdff4414112594edf306c623dec7a6e02411aae
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-12 12:14:53 +0000

    exercicio 14 finalizado

--- exercicio14.py
+++ exercicio14.py
@@ -2,3 +2,13 @@
 # Compras acima de R$ 500,00 têm 10%
 # Acima de R$ 300,00 têm 5%
 # Menor ou igual a R$ 300,00 não têm desconto
+valordacompra = int(input("Digite o valor da sua compra: "))
+
+if valordacompra == 300 :
+    print("Não têm desconto")
+elif valordacompra >= 500:
+    print("Você ganhou um desconto de 10%")
+
+if valordacompra <= 300:
+    print("Não tem desconto")
+# FINALIZADO

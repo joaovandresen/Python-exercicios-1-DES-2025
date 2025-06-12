72fefc688fa9a086b72d2035681aeb9abecf46b3
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-12 11:57:25 +0000

    exercicio 20 finalizado

---
+++ exercicio20.py
@@ -0,0 +1,9 @@
+# Simule um login com nome de usuário "admin" e senha "1234".
+# Caso os dados estejam corretos, exiba "Acesso concedido", senão "Acesso negado".
+usuario = input("Digite seu usuário: ")
+senha = input("Digite sua senha: ")
+
+if usuario == "admin" and senha == "1234":
+        print("Login realizado com sucesso")
+else:
+        print("Acesso negado")

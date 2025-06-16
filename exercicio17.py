60b181ff421f2bbd3a2578b871e7c83be8bcc908
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-16 13:42:36 +0000

    exercicio 17 finalizado

--- exercicio17.py
+++ exercicio17.py
@@ -1,3 +1,13 @@
 #Peça ao usuário uma temperatura em Celsius e converta para Fahrenheit.
 # Fórmula: F = C × 1.8 + 32
 # .
+def celsius_fahrenheit(celsius):
+  
+  fahrenheit = celsius * 1.8 + 32
+  return fahrenheit
+
+temperatura_celsius = float(input("Digite a temperatura em ºC: "))
+
+temperatura_fahrenheit = celsius_fahrenheit(temperatura_celsius)
+print(f"{temperatura_celsius}°C é igual a {temperatura_fahrenheit}°F")
+# FINALIZADO

fea61436af60ffb6bbe9fdc0470d7834bbc27efa
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-16 13:56:56 +0000

    exercicio 19 finalizado

--- exercicio19.py
+++ exercicio19.py
@@ -1,2 +1,11 @@
 # Peça três números e exiba-os em ordem crescente.
-numero = int(input("Digite três numeros: "))
+# Solicita três números ao usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+numeros = [numero1, numero2, numero3]
+
+numeros.sort()
+print("Ordem crescente:", numeros)
+# FINALIZADO

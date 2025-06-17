b694a977e99f750245dbfd0c5735ed8d7a4ea062
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-17 12:13:47 +0000

    carta finalizada

---
+++ carta.py
@@ -0,0 +1,7 @@
+destinatario = input("Digite o nome do destinatário: ")
+mensagem = input("Digite sua mensagem: ")
+remetente = input("Digite seu nome: ")
+
+print("\n--- Sua carta ---")
+print(f"Olá {destinatario}, \n\n{mensagem}\n\nAtenciosamente,\n{remetente}")
+print(f" Email enviado por {remetente} no dia 17/06/25\n")

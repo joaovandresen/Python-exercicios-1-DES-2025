530e2d7171fd6e54e48ceed91f63a9226ee41f7a
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-06-26 11:46:15 +0000

    Exemplo básico finalizado

---
+++ texto.py
@@ -0,0 +1,8 @@
+print("Olá, mundo!")
+
+nome = "Richardson"
+sobrenome = "Schawarski"
+print("Nome completo: " + nome + " " + sobrenome)
+
+idade = 38
+print(f"{nome} {sobrenome} tem {idade} anos.")

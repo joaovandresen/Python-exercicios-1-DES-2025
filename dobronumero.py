65b9c2db50a0064bd197d0ae0dc321ba8cf87b45
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-07-01 12:16:36 +0000

    exercício de dobro finalizado

--- dobronumero.py
+++ dobronumero.py
@@ -1,5 +1,5 @@
-def dobro_numero(n):
+def dobro(n):
     return n * 2
 numero = int(input("Digite o número que você deseja dobrar: "))
-print(f"O dobro desse número é: {dobro_numero(numero)}")
+print(f"O dobro desse número é: {dobro(numero)}")
 # FINALIZADO

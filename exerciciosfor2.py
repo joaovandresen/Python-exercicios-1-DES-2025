c8baac4d4e5e3eb3e06434b14afe5071608d4a1c
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-07-29 13:53:14 +0000

    exercicio for 4 finalizado

---
+++ exerciciosfor2.py
@@ -0,0 +1,3 @@
+programaçao = int(input("Digite um valor maior que 100:"))
+programaçao > 100
+
---
+++ exerciciosfor4.py
@@ -0,0 +1,5 @@
+frutas = ["Maçã", "Banana", "Morango"]
+
+print("Lista de frutas: ")
+for i in range (len(frutas)):
+    print(f"{i + 1} - {frutas[i]}")
